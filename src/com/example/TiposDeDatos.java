9ad485170687f7b293664877de12bfd1b6e27016
Adal Obregon <adalobregoon@gmail.com>
2022-10-10 16:09:37 -0500

    first commit

---
+++ src/com/example/TiposDeDatos.java
@@ -0,0 +1,55 @@
+package com.example;
+
+public class TiposDeDatos {
+    public static void main(String[] args) {
+
+//      enteros
+        byte variable1 = 4;
+        short variable2 = 10;
+        int variable3 = 30;
+        long variable4 = 100;
+
+//      Imprimir enteros
+        System.out.println("Enteros:");
+        System.out.println("byte = " + variable1);
+        System.out.println("short = " + variable2);
+        System.out.println("int = " + variable3);
+        System.out.println("long = " + variable4);
+        System.out.println("=============================");
+
+
+//      decimales
+        float variable5 = 5.5f;
+        double variable6 = 10.5d;
+
+//      Imprimir decimales
+        System.out.println("Decimales:");
+        System.out.println("float = " + variable5);
+        System.out.println("double = " + variable6);
+        System.out.println("=============================");
+
+//      boolean
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+//      Imprimir boolean
+
+        System.out.println("Boolean:");
+        System.out.println("false = " + variable7);
+        System.out.println("true = " + variable8);
+        System.out.println("=============================");
+
+
+
+//      texto
+        char variable9 = 'a';
+        String variable10 = "Adal Obregon";
+
+//      Imprimir Texto
+
+        System.out.println("Texto:");
+        System.out.println("char = " + variable9);
+        System.out.println("string = " + variable10);
+        System.out.println("=============================");
+    }
+}
